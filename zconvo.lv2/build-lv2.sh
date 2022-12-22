9b695b28f859d595166ef061eaafd4334a7b8a41
Christof Laenzlinger <laenzlinger@github.com>
2022-12-23 00:20:30 +0100

    Add lv2 build script

---
+++ zconvo.lv2/build-lv2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p dev/gh
+cd dev/gh
+git clone https://github.com/laenzlinger/zconvo.lv2
+make
+
---
+++ zconvo.lv2/install-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install libfftw3-dev
+apt-get install libsamplerate
+apt-get install libsamplerate-dev
+apt-get install libsndfile-dev
+
+
